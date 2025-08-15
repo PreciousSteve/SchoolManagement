b06b421a8890b9e70213721f7ceadbe188ce4558
Precious Steve <precious.steve@kodehauz.com>
2025-08-15 21:25:22 +0100

    Add db context.

---
+++ SchoolManagement.Persistence/SchoolManagementDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Identity;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagement.Domain;
+using SchoolManagement.Domain.UserManagement;
+
+namespace SchoolManagement.Persistence
+{
+    public class SchoolManagementDbContext : DbContext
+    {
+        public SchoolManagementDbContext(DbContextOptions<SchoolManagementDbContext> options)
+        { 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Classroom> Classrooms { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+    }
+}
