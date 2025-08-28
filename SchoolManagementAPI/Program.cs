ed2cf4279cbbf53e96e637037d315962f06c54ae
Precious Steve <precious.steve@kodehauz.com>
2025-08-29 00:12:56 +0100

    Add authentication settings.

--- SchoolManagementAPI/Program.cs
+++ SchoolManagementAPI/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using SchoolManagement.Persistence;
 using System.Text.Json.Serialization;
 
@@ -21,8 +23,57 @@ builder.Services.AddDbContext<SchoolManagementDbContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.AddSecurityDefinition()
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Enter token in the text below"
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference =new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }   
+    });
 });
+
+// Add authentication service
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Issuer"],
+        ValidAudience = builder.Configuration["Audience"],
+        //IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        ClockSkew = TimeSpan.FromHours(2)
+    };
+});
+
+
+//// Add the authorization service
+//builder.Services.AddAuthorizationBuilder()
+//    .AddPolicy("Admin", policy => policy.RequireRole(AccountType.Admin.ToString()))
+//    .AddPolicy("User", policy => policy.RequireRole(AccountType.User.ToString()));
+
 
 var app = builder.Build();
 
