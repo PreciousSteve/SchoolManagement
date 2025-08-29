b7b1cd62cbadf1aa885b06ec9ac7b4a69f10f649
Precious Steve <precious.steve@kodehauz.com>
2025-08-29 15:49:18 +0100

    Update Teacher endpoints.

--- SchoolManagementAPI/Controllers/TeacherController.cs
+++ SchoolManagementAPI/Controllers/TeacherController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using SchoolManagement.Domain.UserManagement;
 using SchoolManagement.Dto;
 using SchoolManagement.Persistence;
+using System.Security.Claims;
 
 namespace SchoolManagement.API.Controllers
 {
@@ -40,6 +41,12 @@ namespace SchoolManagement.API.Controllers
         [HttpPut("{id}")]
         public async Task<ActionResult<Teacher>> UpdateTeacher(int id, TeacherCreateDto dto)
         {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            var role = identity.FindFirst(ClaimTypes.Role).Value;
+            if (role != "Teacher")
+            {
+                return Unauthorized("You cannot Update this Teacher");
+            }
             var teacher = await _context.Teachers.FindAsync(id);
             if (teacher == null)
             {
@@ -75,6 +82,12 @@ namespace SchoolManagement.API.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteTeacher(int id)
         {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            var role = identity.FindFirst(ClaimTypes.Role).Value;
+            if (role != "Teacher")
+            {
+                return Unauthorized("You cannot Delete this Teacher");
+            }
             var teacher = await _context.Teachers.FindAsync(id);
             if (teacher == null)
             {
